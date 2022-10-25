20d87ad95725973e3d4db9820d6869ef3e1d1ce0
Anthony <anthony560@hotmail.com>
2022-10-25 15:02:16 +0200

    add ISR

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -3,11 +3,16 @@ import { pokeApi } from '../api';
 
 export const getPokemonInfo = async ( nameOrId:string ) => {
 
-    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ nameOrId }`)
-
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+    try {
+        const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ nameOrId }`)
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
+    } catch (error) {
+        return null
     }
+
+
 }
